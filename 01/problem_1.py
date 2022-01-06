9e33f6c4dbd822557cfd0b0cddb80e1724441c8d
David A. Ventimiglia <davidaventimiglia@neptunestation.com>
2022-01-06 13:41:04 -0800

    Minor changes

--- 01/problem_1.py
+++ 01/problem_1.py
@@ -1,3 +1,5 @@
+"""Problem 1."""
+
 # If we list all the natural numbers below 10 that are multiples of 3
 # or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
 
@@ -9,10 +11,8 @@ def sumargs(*args):
     return sum(args)
 
 
-def multiples(limit, *args):
-    sorted(args, reverse=True)
-    
-    
+# def multiples(limit, *args):
+#     sorted(args, reverse=True)
 
 
 def multiples(base=1, limit=10):
